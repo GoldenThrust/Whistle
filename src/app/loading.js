31f742c9e85d886e1975efdb77f10db6a5a53905
Adeniji Olajide <adenijiolajid01@gmail.com>
2025-05-03 06:22:50 +0100

    load screen

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,10 +1,5 @@
 export default function Loading() {
-    return <>
-        <div>
-            Whistle
-        </div>
-        <div>
-            Meteorology Code
-        </div>
-    </>
+    return <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-lg md:text-3xl">
+        <span className="italic font-extrabold">Whistle</span> -  Meteorology Code
+    </div>
 }
